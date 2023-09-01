bac40efff9e5721c45fd4c0f4b8274314d22ff8e
George Hlongwane <georgehlongwane8@gmail.com>
2023-09-01 11:23:13 +0200

    Added more product templates

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from django.db import models
-from PIL import Image
 from io import BytesIO
 from msilib.schema import File
+from PIL import Image
+from django.db import models
+
 
 class Category(models.Model):
     name = models.CharField(max_length=250)
@@ -23,19 +24,20 @@ class Product(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+   
 
     class Meta:
         ordering = ('-created_on',)
 
     def __str__(self):
-            return self.name
-
+        return self.name
+    
     def price_display(self):
         return self.price / 100
 
     def get_thumbnail(self):
         if self.thumbnail:
-            return self.thumbnail.url   
+            return self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_tbn(self.image)
@@ -45,14 +47,16 @@ class Product(models.Model):
             else:
                 return 'https://via.placeholder.com/350x150'
 
-            def make_tbn(self, image, size=(300, 300)):
-                img = Image.open(image)
-                img.convert('RGB')
-                img.thumbnail(size)
+    def make_tbn(self, image, size=(300, 300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
 
-                tbn_io = BytesIO()
-                img.save(tbn_io, 'JPEG', quality=85)
+        tbn_io = BytesIO()
+        img.save(tbn_io, 'JPEG', quality=85)
 
-                thumbnail = File(tbn_io, name=image.name)
+        thumbnail = File(tbn_io, name=image.name)
 
-                return thumbnail
+        return thumbnail
+
+        
