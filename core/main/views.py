bc6b7efefa09f7a2308754888305a274faee91fb
George Hlongwane <georgehlongwane8@gmail.com>
2023-09-01 12:51:52 +0200

    Added a new product to the project

--- core/main/views.py
+++ core/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from products.views import Product
 
 def index(request):
-
-    return render(request, "main/index.html")
+    products = Product.objects.all()[0:5]
+    return render(request, "main/index.html", {'products':products})
--- core/products/views.py
+++ core/products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    return render(request, 'products/product.html', {'product': product})
